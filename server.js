7329f0d3c06bfd69079e101484d22260d0e52bd3
PPDLM <a01640791@tec.mx>
2026-04-21 23:49:22 -0600

    fix: use dynamic port for azure

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+// Change this line:
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-console.log(`Server running at http://localhost:${PORT}`);
+// Added '0.0.0.0' so it listens to external requests
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
